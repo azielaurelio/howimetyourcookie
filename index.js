41316c84aad57bb1b4c7ec0c92a04cb150e9725d
Aziel Aurelio <169033394+azielaurelio@users.noreply.github.com>
2024-12-17 16:04:01 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function redirIg(){
+    window.open("https://www.instagram.com/howimetyourcookie/","_blank");
+}
+
+function redirTiktok(){
+   window.open("https://www.tiktok.com/@howimetyourcookie?lang=id-ID","_blank");
+}
